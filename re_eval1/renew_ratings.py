adadf988b870d607f0f1eecf179ef20ba68a99d2
Wisdom Okwen <wisdomokwen@gmail.com>
2025-04-25 10:46:18 -0400

    Log overall ratings for gpt

--- re_eval1/renew_ratings.py
+++ re_eval1/renew_ratings.py
@@ -1,6 +1,7 @@
 import os
 import csv
-from critiques import (
+from analysis.utility import get_low_overall_rating_ids
+from analysis.gpt_ratings.critiques import (
     get_gpt_overall_rating, 
     SELF_ASSESSMENT_PROMPT, 
     USER_ASSESSMENT_PROMPT, 
@@ -8,8 +9,8 @@ from critiques import (
 )
 
 
-OVER_ALL_RATINGS_FILE = "/playpen-ssd/wokwen/projects/chatbot_eval/overall_ratings.csv"
-CONVO_DIR = "../conv_trajectories/conv_trajectories"
+OVER_ALL_RATINGS_FILE = "/playpen-ssd/wokwen/projects/chatbot_eval/re_eval1/gpt/ratings/overall_ratings.csv"
+OVERALL_CONVO_DIR = "/playpen-ssd/wokwen/projects/chatbot_eval/re_eval1/gpt/new_convs_overall"
 CONVO_PREFIX = "trajectory"
 
 
@@ -19,18 +20,18 @@ headers = ["Convsation_Id", "User_Rating", "Observer_Rating", "Self_Rating"]
 #     writer = csv.writer(file)
 #     writer.writerow(headers)
 
+low_rated_overall_convs = get_low_overall_rating_ids()
        
 def generate_overall_ratings():
-
     write_headers = not os.path.exists(OVER_ALL_RATINGS_FILE) or os.stat(OVER_ALL_RATINGS_FILE).st_size == 0
     with open(OVER_ALL_RATINGS_FILE, mode="a", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
         if write_headers:
             writer.writerow(headers)
 
-        for i in range(530, 531):
-            convo_file_name = f"{CONVO_PREFIX}{i}.csv"
-            convo_file_path = os.path.join(CONVO_DIR, convo_file_name)
+        for id in low_rated_overall_convs:
+            convo_file_name = f"{CONVO_PREFIX}{id}.csv"
+            convo_file_path = os.path.join(OVERALL_CONVO_DIR, convo_file_name)
 
             if not os.path.exists(convo_file_path):
                 print(f"Missing: {convo_file_path}")
@@ -50,8 +51,11 @@ def generate_overall_ratings():
             observer_rating = get_gpt_overall_rating(THIRD_PARTY_ASSESSMENT_PROMPT, formatted_convo)
             self_rating = get_gpt_overall_rating(SELF_ASSESSMENT_PROMPT, formatted_convo)
 
-            writer.writerow([i, user_rating, observer_rating, self_rating])
-            print(f"Rated convo {i}: User={user_rating}, Observer={observer_rating}, Self={self_rating}")
+            writer.writerow([id, user_rating, observer_rating, self_rating])
+            print(f"Rated convo {id}: User={user_rating}, Observer={observer_rating}, Self={self_rating}")
+
+def get_llama_overall_ratings():
+    pass
 
 
 if __name__ == '__main__':
